d6e106c2475671ba7c917be5cf8b01ecb9f651aa
Aryansh V <emailtoav09@gmail.com>
2026-04-26 23:45:33 +0530

    completed About section with desktop and mobile view

--- src/LandingPage/3.About/About.jsx
+++ src/LandingPage/3.About/About.jsx
@@ -2,50 +2,68 @@ import "./About.scss";
 import { useState, useEffect, useRef } from "react";
 
 const About = () => {
-  const slides = ["gg", "hh"];
-
-  const fakeSlides = [
-    slides[slides.length - 1],
-    ...slides,
-    slides[0]
-  ];
-
-  const [currSlide, changeSlide] = useState(1);
-  const [animate, setAnimate] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      changeSlide((prev) => prev + 1);
-      setAnimate(true);
-    }, 2500);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const handleTransitionEnd = () => {
-    if (currSlide === fakeSlides.length - 1) {
-      setAnimate(false);
-      changeSlide(1);
-    }
-  };
 
   return (
     <div id="AboutMC">
-      <div className="slidSh">
-        <div
-          className="track"
-          onTransitionEnd={handleTransitionEnd}
-          style={{
-            transform: `translateX(-${currSlide * 100}%)`,
-            transition: animate ? "transform 0.8s ease" : "none"
-          }}
-        >
-          {fakeSlides.map((item, index) => (
-            <div className="slideCon" key={index}>
-              {item}
-            </div>
-          ))}
+      <h1>What We Offer</h1>
+      <div className="GridCon">
+
+        <div className="Con ACone">
+          <img src="/InvestInsight.png" alt=""/>
+
+          <h3>Your legacy deserves a solid foundation. DreamLadder Capital helps you build lasting wealth through smart,
+            diversified strategies.</h3>
+          <img src="/tele.png" alt=""/>
         </div>
+
+        <div className="Con ACtwo">
+          <div className="ContwoEles">
+            <h2>PMS / AIF</h2>
+            <h3>Access Curated Portfolios from top Fund Managers</h3>
+          </div>
+          <div className="ContwoEles">
+            <h2>Mutual Fund</h2>
+            <h3>Choose and Invest Through Leading Funds</h3>
+          </div>
+          <div className="ContwoEles">
+            <h2>Bond / Corporate FD</h2>
+            <h3>Invest in High-Quality Products</h3>
+          </div>
+          <div className="ContwoEles">
+            <h2>Structured Products</h2>
+            <h3>Facilitated Access to Market-Linked Solutions</h3>
+          </div>
+          <div className="ContwoEles">
+            <h2>Legacy & Estate Planning</h2>
+            <h3>Curated Solutions for Wealth Transfer</h3>
+          </div>
+          <div className="ContwoEles">
+            <h2>Insurance</h2>
+            <h3>Access Life, Health, and Asset Coverage</h3>
+          </div>
+        </div>
+
+        <div className="Con ACthree"><img src="/profileDashboard.png" alt=""/></div>
+
+        <div className="Con ACfour">
+          <h2>All Your Family Investments, One Simple App</h2>
+          <h3>Easily view portfolio performance, transactions and reports for every family member - anytime, anywhere</h3>
+          <button className="navBtn s">Start now</button>
+        </div>
+
+        <div className="Con ACfive">
+          <h2>Setup SIP in 2 Minutes</h2>
+          <h3>Setup SIP in Mutual Funds and diversify your portfolio to grow your net worth</h3>
+          <div className="IL1">
+            <button className="navBtn s">Setup SIP</button>
+            <button className="navBtn s">SIP Calculator</button>
+          </div>
+        </div>
+
+        <div className="Con ACsix">
+          <img src="/sipAmount.png" alt=""/>
+        </div>
+
       </div>
     </div>
   );
