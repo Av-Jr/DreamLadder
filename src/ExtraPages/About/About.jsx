6af26cf290c98a8b0d305fda45b2a032fd798382
Aryansh V <emailtoav09@gmail.com>
2026-05-08 15:26:58 +0530

    naming issues of classes causing disalignment in landing page about section and section page solved

--- src/ExtraPages/About/About.jsx
+++ src/ExtraPages/About/About.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div id={"AboutMC"}>
+        <div id={"About2MC"}>
             <NavBar></NavBar>
             <div className="firstPage">
                 <span className={"main"}>
