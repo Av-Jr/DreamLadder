e53553837e1ca5f19465ad25d397a9862da9d727
Av . <av.@Avs-MacBook-Air.local>
2026-07-28 02:42:16 +0530

    FIX : added lazy loading to different pages for reducing loading time

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,57 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Faqs from "./ExtraPages/Faqs/Faqs.jsx";
-import CalcGraph from "./ExtraPages/CalcGraph/CalcGraph.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+
 import Home from "./Home.jsx";
-import About from "./ExtraPages/About/About.jsx";
-import Wws from "./ExtraPages/WhoWeServe/Wws.jsx"
-import MFs from "./ExtraPages/MFs/MFs.jsx";
-import PMSAIF from "./ExtraPages/PMSAIF/PMSAIF.jsx"
-import Bonds from "./ExtraPages/Bonds/Bonds.jsx";
-import StructProd from "./ExtraPages/StructProd/StructProd.jsx";
-import LEP from "./ExtraPages/LEP/LEP.jsx";
-import Insurance from "./ExtraPages/Insurance/Insurance.jsx";
-import Connect from "./ExtraPages/ConnectUs/Connect.jsx";
-import Bp from "./ExtraPages/Bp/Bp.jsx";
-import Blogs from "./ExtraPages/Blogs/Blogs.jsx";
-import Media from "./ExtraPages/Media/Media.jsx";
-import Disclosure from "./ExtraPages/Disclosure/Disclosure.jsx";
-import Disclaimer from "./ExtraPages/Disclaimer/Disclaimer.jsx";
-import PrivacyPolicy from "./ExtraPages/PrivacyPolicy/PrivacyPolicy.jsx";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
- import ScrollToTop from "./ExtraPages/ScrollToTop.jsx";
+import ScrollToTop from "./ExtraPages/ScrollToTop.jsx";
+
+// Lazy-loaded pages
+const Faqs = lazy(() => import("./ExtraPages/Faqs/Faqs.jsx"));
+const CalcGraph = lazy(() => import("./ExtraPages/CalcGraph/CalcGraph.jsx"));
+const About = lazy(() => import("./ExtraPages/About/About.jsx"));
+const Wws = lazy(() => import("./ExtraPages/WhoWeServe/Wws.jsx"));
+const MFs = lazy(() => import("./ExtraPages/MFs/MFs.jsx"));
+const PMSAIF = lazy(() => import("./ExtraPages/PMSAIF/PMSAIF.jsx"));
+const Bonds = lazy(() => import("./ExtraPages/Bonds/Bonds.jsx"));
+const StructProd = lazy(() => import("./ExtraPages/StructProd/StructProd.jsx"));
+const LEP = lazy(() => import("./ExtraPages/LEP/LEP.jsx"));
+const Insurance = lazy(() => import("./ExtraPages/Insurance/Insurance.jsx"));
+const Connect = lazy(() => import("./ExtraPages/ConnectUs/Connect.jsx"));
+const Bp = lazy(() => import("./ExtraPages/Bp/Bp.jsx"));
+const Blogs = lazy(() => import("./ExtraPages/Blogs/Blogs.jsx"));
+const Media = lazy(() => import("./ExtraPages/Media/Media.jsx"));
+const Disclosure = lazy(() => import("./ExtraPages/Disclosure/Disclosure.jsx"));
+const Disclaimer = lazy(() => import("./ExtraPages/Disclaimer/Disclaimer.jsx"));
+const PrivacyPolicy = lazy(() => import("./ExtraPages/PrivacyPolicy/PrivacyPolicy.jsx"));
 
 const App = () => {
-  return(
-      <>
-          <BrowserRouter>
-              <ScrollToTop></ScrollToTop>
-              <Routes>
-                  <Route path={"/"} element={<Home />}></Route>
-                  <Route path={'/faqs'} element={<Faqs />}></Route>
-                  <Route path={'/calc'} element={<CalcGraph></CalcGraph>}></Route>
-                  <Route path={"/about"} element={<About></About>}></Route>
-                  <Route path={"/who-we-serve"} element={<Wws />}></Route>
-                  <Route path={"/mutual-funds"} element={<MFs></MFs>}></Route>
-                  <Route path={"/pms"} element={<PMSAIF/>}></Route>
-                  <Route path={"/bonds"} element={<Bonds />}></Route>
-                  <Route path={"/structured-products"} element={<StructProd />}></Route>
-                  <Route path={"/legacy-estate-planning"} element={<LEP />}></Route>
-                  <Route path={"/Insurance"} element={<Insurance />}></Route>
-                  <Route path={"/connect"} element={<Connect></Connect>}></Route>
-                  <Route path={"/bespoke-portfolio"} element={<Bp />}></Route>
-                  <Route path={'/blogs'} element={<Blogs />}></Route>
-                  <Route path={'/media'} element={<Media />}></Route>
-                  <Route path={'/disclosure'} element={<Disclosure />}></Route>
-                  <Route path={'/disclaimer'} element={<Disclaimer />}></Route>
-                  <Route path={'/privacy-policy'} element={<PrivacyPolicy />}></Route>
-              </Routes>
-          </BrowserRouter>
-      </>
-  )
-}
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
 
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/faqs" element={<Faqs />} />
+                    <Route path="/calc" element={<CalcGraph />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/who-we-serve" element={<Wws />} />
+                    <Route path="/mutual-funds" element={<MFs />} />
+                    <Route path="/pms" element={<PMSAIF />} />
+                    <Route path="/bonds" element={<Bonds />} />
+                    <Route path="/structured-products" element={<StructProd />} />
+                    <Route path="/legacy-estate-planning" element={<LEP />} />
+                    <Route path="/Insurance" element={<Insurance />} />
+                    <Route path="/connect" element={<Connect />} />
+                    <Route path="/bespoke-portfolio" element={<Bp />} />
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route path="/media" element={<Media />} />
+                    <Route path="/disclosure" element={<Disclosure />} />
+                    <Route path="/disclaimer" element={<Disclaimer />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+};
 
 export default App;
